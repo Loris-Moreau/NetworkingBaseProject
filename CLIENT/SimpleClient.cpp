511640d9a76834299b419339420a188148c69fa3
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2024-04-29 10:54:25 +0200

    [fix] client

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -1,16 +1,60 @@
+
+#include <iostream>
 #include <SDL.h>
 #include <SDL_net.h>
-#include <iostream>
 //#include <string>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
-	if (SDLNet_Init() == -1) {
-		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+int main(int argc, char* argv[])
+{
+	if (SDLNet_Init() == -1)
+	{
+		cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
 		return 1;
 	}
+	IPaddress ip;
+	if (SDLNet_ResolveHost(&ip, "localhost" , 4242) == -1)
+	{
+		cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
+		SDLNet_Quit();
+		return 1;
+	}
+	const TCPsocket clientSocket= SDLNet_TCP_Open(&ip);
+	if (!clientSocket)
+	{
+		cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
+		SDLNet_Quit();
+		return 1;
+	}
+	string message;
+	cout << "Message : ";
+	cin >> message;
+	
+	const int bytesSent = SDLNet_TCP_Send(clientSocket, message.c_str(), message.length() + 1);
+	if (bytesSent < message.length() + 1)
+	{
+		cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_Quit();
+		return 1;
+	}
+	
+	cout << "Sent " << bytesSent << " bytes to the server" << '\n';
 
-	cout << "Thank you for using ChArtFX !\n";
+	char buffer[1024];
+	const int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+	if (bytesRead <= 0)
+	{
+		cerr << "SDLNet TCP Recv error : " << SDLNet_GetError() << '\n';
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_Quit();
+		return 1;
+	}
+	
+	cout << "Incoming response : " << buffer << '\n';
+	
+	//SDLNet_TCP_Close(clientSocket);
+	//SDLNet_Quit();
+	
 	return 0;
 }
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -1,15 +1,64 @@
+#include <iostream>
 #include <SDL.h>
 #include <SDL_net.h>
-#include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
-	if (SDLNet_Init() == -1) {
-		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+int main(int argc, char* argv[])
+{
+	if (SDLNet_Init() == -1)
+	{
+		cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
 		return 1;
 	}
-
-	cout << "Thank you for using ChArtFX !\n";
+	IPaddress ip;
+	if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1)
+	{
+		cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
+		SDLNet_Quit();
+		return 1;
+	}
+	const TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
+	if (!serverSocket)
+	{
+		cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
+		SDLNet_Quit();
+		return 1;
+	}
+	
+	TCPsocket clientSocket;
+	while (true)
+	{
+		clientSocket = SDLNet_TCP_Accept(serverSocket);
+		if (clientSocket)
+		{
+			cout << "A client reached the server" << '\n';
+			char buffer[1024];
+			int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+			if (bytesRead > 0)
+			{
+				string answer;
+				cout << "Incoming message : " << buffer << '\n';
+				if(bytesRead == 7)
+				{
+					answer = "Doom Bot - status : activated";
+				}
+				else if()
+				{
+						
+				}
+				else
+				{
+					answer = "Message received";
+				}
+				const int bytesSent = SDLNet_TCP_Send(clientSocket, answer.c_str(), answer.length() + 1);
+				if (bytesSent < answer.length() + 1)
+				{
+					cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
+					break;
+				}
+			}
+		}
+	}
+	
 	return 0;
 }
