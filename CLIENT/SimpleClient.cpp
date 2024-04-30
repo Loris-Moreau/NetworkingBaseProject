f13753b188c76e4a902e34eb1231115684327445
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2024-04-30 10:52:12 +0200

    [fix] script blocking bug

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -4,119 +4,122 @@
 #include <SDL_net.h>
 #include <string>
 #include <vector>
-//#include <string>
+
 using namespace std;
 
-struct Message
-{
-	bool fromMe = false;
-	string content;
+struct Message {
+    string userName = "idiot";
+    bool fromMe = false;
+    string content;
 };
 
-void initWindow()
-{
-	vector<Message> log{Message{false, "Waiting for someone to talk to..."}};
-	
-	const int width = 500, height = 750;
-	InitWindow(width, height, "mySpace V2");
-	//SetTargetFPS(60);
-	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
-	
-	string typing;
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(GRAY);
-		DrawText("Welcome to ChArtFX!", 220, 15, 25, WHITE);
-		DrawRectangle(20, 50, width-40, height -150, DARKGRAY);
-		DrawRectangle(20, height - 90, width-40, 50, LIGHTGRAY);
-		
-		DrawRectangle(20, 50, width-40, height -150, DARKGRAY);
-
-		for(int msg = 0; msg < log.size(); msg++)
-		{
-			DrawText(log[msg].content.c_str(), 30, 75 + (msg*30), 15, log[msg].fromMe?SKYBLUE:PURPLE);
-		}
-		
-		DrawRectangle(20, height - 90, width-40, 50, LIGHTGRAY);
-		int inputChar = GetCharPressed();
-		if(inputChar != 0) //A character is pressed on the keyboard
-		{
-			typing += static_cast<char>(inputChar);
-		}
-		if(typing.size() > 0)
-		{
-			DrawText(typing.c_str(), 30, height - 75, 25, DARKBLUE);
-		}
-		if(typing.size() > 0)
-		{
-			if(IsKeyPressed(KEY_BACKSPACE)) typing.pop_back();
-			else if (IsKeyPressed(KEY_ENTER))
-			{
-				//Send the message typing to the server here!
-				log.push_back(Message{true, typing});
-				typing.clear();
-			}
-			DrawText(typing.c_str(), 30, height - 75, 25, DARKBLUE);
-		}
-		EndDrawing();
-	}
-	CloseWindow();
+string GetUserName() {
+    string _name = "Anon";
+    return _name;
 }
 
-int main(int argc, char* argv[])
-{
-	if (SDLNet_Init() == -1)
-	{
-		cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
-		return 1;
-	}
-	IPaddress ip;
-	if (SDLNet_ResolveHost(&ip, "localhost" , 4242) == -1)
-	{
-		cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
-		SDLNet_Quit();
-		return 1;
-	}
-	const TCPsocket clientSocket= SDLNet_TCP_Open(&ip);
-	if (!clientSocket)
-	{
-		cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
-		SDLNet_Quit();
-		return 1;
-	}
-	
-	initWindow();
-	
-	string message;
-	/*cout << "Message : ";
-	getline(cin, message);*/
-	
-	const int bytesSent = SDLNet_TCP_Send(clientSocket, message.c_str(), message.length() + 1);
-	if (bytesSent < message.length() + 1)
-	{
-		cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
-		SDLNet_TCP_Close(clientSocket);
-		SDLNet_Quit();
-		return 1;
-	}
-	
-	cout << "Sent " << bytesSent << " bytes to the server" << '\n';
+int main(int argc, char* argv[]) {
+    // Initialize SDL_net
+    if (SDLNet_Init() == -1) {
+        cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
+        return 1;
+    }
 
-	char buffer[1024];
-	const int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
-	if (bytesRead <= 0)
-	{
-		cerr << "SDLNet TCP Recv error : " << SDLNet_GetError() << '\n';
-		SDLNet_TCP_Close(clientSocket);
-		SDLNet_Quit();
-		return 1;
-	}
-	
-	cout << "Incoming response : " << buffer << '\n';
-	
-	//SDLNet_TCP_Close(clientSocket);
-	//SDLNet_Quit();
+    // Resolve the server's IP address and port
+    IPaddress ip;
+    if (SDLNet_ResolveHost(&ip, "localhost" , 4242) == -1) {
+        cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
+        SDLNet_Quit();
+        return 1;
+    }
 
-	return 0;
+    // Open a TCP connection to the server
+    const TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
+    if (!clientSocket) {
+        cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
+        SDLNet_Quit();
+        return 1;
+    }
+
+    // Initialize the window
+    constexpr int width = 500, height = 750;
+    InitWindow(width, height, "Chat Client");
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+    // Vector to store chat log
+    vector<Message> log{Message{GetUserName(), false, "Waiting for someone to talk to..."}};
+
+    // String to store user input
+    string typing;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        // Draw UI elements
+        DrawText("Welcome to ChArtFX!", 220, 15, 25, WHITE);
+        DrawRectangle(20, 50, width - 40, height - 150, DARKGRAY);
+        DrawRectangle(20, height - 90, width - 40, 50, LIGHTGRAY);
+
+        // Draw chat log
+        for (int msg = 0; msg < log.size(); msg++)
+        {
+            DrawText(log[msg].content.c_str(), 30, 75 + (msg * 30), 15, log[msg].fromMe ? SKYBLUE : PURPLE);
+        }
+
+        // Draw user input
+        DrawText(typing.c_str(), 30, height - 75, 25, DARKBLUE);
+
+        EndDrawing();
+
+        // Handle user input
+        int inputChar = GetCharPressed();
+        if (inputChar != 0) { // A character is pressed on the keyboard
+            typing += static_cast<char>(inputChar);
+        }
+        if (!typing.empty()) {
+            if (IsKeyPressed(KEY_BACKSPACE)) typing.pop_back();
+            else if (IsKeyPressed(KEY_ENTER))
+            {
+                // Send the message typing to the server here!
+                string message = typing;
+
+                const int bytesSent = SDLNet_TCP_Send(clientSocket, message.c_str(), message.length() + 1);
+                if (bytesSent < message.length() + 1) {
+                    cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
+                    SDLNet_TCP_Close(clientSocket);
+                    SDLNet_Quit();
+                    return 1;
+                }
+
+                cout << "Sent " << bytesSent << " bytes to the server" << '\n';
+
+                SDLNet_SocketSet socketSet = SDLNet_AllocSocketSet(1);
+                SDLNet_AddSocket(socketSet, reinterpret_cast<SDLNet_GenericSocket>(clientSocket));
+                if (SDLNet_CheckSockets(socketSet, 0) != 0)
+                {
+                    char buffer[1024];
+                    int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+                    if (bytesRead <= 0)
+                    {
+                        cerr << "SDLNet TCP Recv error : " << SDLNet_GetError() << '\n';
+                        SDLNet_TCP_Close(clientSocket);
+                        SDLNet_Quit();
+                        return 1;
+                    }
+
+                    cout << "Incoming response : " << buffer << '\n';
+}
+                    log.push_back(Message{GetUserName(), true, typing});
+                    typing.clear();
+                
+            }
+        }
+    }
+
+    // Close the window and quit SDL_net
+    CloseWindow();
+    SDLNet_TCP_Close(clientSocket);
+    SDLNet_Quit();
+    return 0;
 }
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -1,65 +1,84 @@
 #include <iostream>
-#include <regex>
+#include <string>
 #include <SDL.h>
 #include <SDL_net.h>
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (SDLNet_Init() == -1)
-	{
-		cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
-		return 1;
-	}
-	IPaddress ip;
-	if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1)
-	{
-		cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
-		SDLNet_Quit();
-		return 1;
-	}
-	const TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
-	if (!serverSocket)
-	{
-		cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
-		SDLNet_Quit();
-		return 1;
-	}
-	
-	TCPsocket clientSocket;
-	while (true)
-	{
-		clientSocket = SDLNet_TCP_Accept(serverSocket);
-		if (clientSocket)
-		{
-			cout << "A client reached the server" << '\n';
-			char buffer[1024];
-			int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
-			if (bytesRead > 0)
-			{
-				string answer;
-				cout << "Incoming message : " << buffer << '\n';
-				if(bytesRead == 7)
-				{
-					answer = "Doom Bot - status : activated";
-				}
-				else if(strcmp(buffer, "Hello there") != 0)
-				{
-					answer = "General Kenobi";
-				}
-				else
-				{
-					answer = "Stop bothering me";
-				}
-				const int bytesSent = SDLNet_TCP_Send(clientSocket, answer.c_str(), answer.length() + 1);
-				if (bytesSent < answer.length() + 1)
-				{
-					cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
-					break;
-				}
-			}
-		}
-	}
-	
-	return 0;
+    if (SDLNet_Init() == -1)
+    {
+        cerr << "SDLNet_Init error : " << SDLNet_GetError() << '\n';
+        return 1;
+    }
+    IPaddress ip;
+    if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1)
+    {
+        cerr << "Resolve Host error : " << SDLNet_GetError() << '\n';
+        SDLNet_Quit();
+        return 1;
+    }
+    const TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
+    if (!serverSocket)
+    {
+        cerr << "TCP Open error : " << SDLNet_GetError() << '\n';
+        SDLNet_Quit();
+        return 1;
+    }
+    SDLNet_SocketSet set = SDLNet_AllocSocketSet(8);
+    if (!set)
+    {
+        cerr << "Socket set allocation error : " << SDLNet_GetError() << '\n';
+        SDLNet_Quit();
+        return 1;
+    }
+
+    while (true)
+    {
+        TCPsocket clientSocket = SDLNet_TCP_Accept(serverSocket);
+
+        if (clientSocket)
+        {
+            IPaddress* clientIP = SDLNet_TCP_GetPeerAddress(clientSocket);
+            Uint32 ipAddress = SDLNet_Read32(clientIP);
+            cout << "Client " << ipAddress << " reached the server" << '\n';
+
+            SDLNet_AddSocket(set, reinterpret_cast<SDLNet_GenericSocket>(clientSocket));
+
+            char buffer[1024];
+
+            if (SDLNet_CheckSockets(set, 10) != 0) // Timeout of 10ms
+            { 
+                int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+                if (bytesRead > 0)
+                {
+                    string answer;
+                    cout << "Incoming message : " << buffer << '\n';
+                    if (bytesRead == 7)
+                    {
+                        answer = "Doom Bot - status : activated";
+                    }
+                    else if (buffer == "Hello there")
+                    {
+                        answer = "General Kenobi";
+                    }
+                    else
+                    {
+                        answer = "Stop";
+                    }
+                    const int bytesSent = SDLNet_TCP_Send(clientSocket, answer.c_str(), answer.length() + 1);
+                    if (bytesSent < answer.length() + 1)
+                    {
+                        cerr << "SDLNet TCP Send error : " << SDLNet_GetError() << '\n';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    SDLNet_FreeSocketSet(set);
+    SDLNet_Quit();
+    return 0;
 }
